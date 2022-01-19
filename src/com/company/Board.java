7ecc51f4ea4fcf4d6365df21817f8270692ec91d
daniel haddad <daniel.haddad@yahooinc.com>
2022-01-19 20:17:05 +0200

    added move logic

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -12,14 +12,19 @@ import java.util.stream.Collectors;
 public class Board {
     
     Piece[][] matrix = {
-        {new Bishop("white", new Point(0,0)),new Bishop("white", new Point(1,0)),new Bishop("white", new Point(2,0)),new Bishop("white", new Point(3,0)),new Bishop("white", new Point(4,0)),new Bishop("white", new Point(5,0)),new Bishop("white", new Point(6,0)),new Bishop("white", new Point(7,0))},
-        {new Pawn("white", new Point(0, 1)),new Pawn("white", new Point(1, 1)),new Pawn("white", new Point(2, 1)),new Pawn("white", new Point(3, 1)),new Pawn("white", new Point(4, 1)),new Pawn("white", new Point(5, 1)),new Pawn("white", new Point(6, 1)),new Pawn("white", new Point(7, 1)),},
+        {new Bishop("white", new Point(0, 0)), new Bishop("white", new Point(1, 0)), new Bishop("white", new Point(2, 0)), new Bishop("white", new Point(3, 0)),
+         new Bishop("white", new Point(4, 0)), new Bishop("white", new Point(5, 0)), new Bishop("white", new Point(6, 0)), new Bishop("white", new Point(7, 0))},
+        {new Pawn("white", new Point(0, 1)), new Pawn("white", new Point(1, 1)), new Pawn("white", new Point(2, 1)), new Pawn("white", new Point(3, 1)),
+         new Pawn("white", new Point(4, 1)), new Pawn("white", new Point(5, 1)), new Pawn("white", new Point(6, 1)), new Pawn("white", new Point(7, 1)),},
         {null, null, null, null, null, null, null, null},
         {null, null, null, null, null, null, null, null},
         {null, null, null, null, null, null, null, null},
         {null, null, null, null, null, null, null, null},
-        {new Bishop("white", new Point(0,6)),new Bishop("white", new Point(1,6)),new Bishop("white", new Point(2,6)),new Bishop("white", new Point(3,6)),new Bishop("white", new Point(4,6)),new Bishop("white", new Point(5,6)),new Bishop("white", new Point(6,6)),new Bishop("white", new Point(7,6)),new Bishop("white", new Point(0,0))},
-        {new Pawn("white", new Point(0, 7)),new Pawn("white", new Point(1, 7)),new Pawn("white", new Point(2, 7)),new Pawn("white", new Point(3, 7)),new Pawn("white", new Point(4, 7)),new Pawn("white", new Point(5, 7)),new Pawn("white", new Point(6, 7)),new Pawn("white", new Point(7, 5)),}
+        {new Bishop("white", new Point(0, 6)), new Bishop("white", new Point(1, 6)), new Bishop("white", new Point(2, 6)), new Bishop("white", new Point(3, 6)),
+         new Bishop("white", new Point(4, 6)), new Bishop("white", new Point(5, 6)), new Bishop("white", new Point(6, 6)), new Bishop("white", new Point(7, 6)),
+         new Bishop("white", new Point(0, 0))},
+        {new Pawn("white", new Point(0, 7)), new Pawn("white", new Point(1, 7)), new Pawn("white", new Point(2, 7)), new Pawn("white", new Point(3, 7)),
+         new Pawn("white", new Point(4, 7)), new Pawn("white", new Point(5, 7)), new Pawn("white", new Point(6, 7)), new Pawn("white", new Point(7, 5)),}
     };
     
     public Board() {
@@ -60,21 +65,39 @@ public class Board {
                 }
             }
         }
-        if (piece instanceof King){
-            points = (ArrayList<Point>) points.stream().filter(s -> !isWin(new Board(this).move(location,s))).collect(Collectors.toList());
+        if (piece instanceof King) {
+            points = (ArrayList<Point>) points.stream().filter(s -> {
+                try {
+                    Board tempBoard = new Board(this);
+                    tempBoard._move(location, s);
+                    return !isWin(tempBoard);
+                } catch (Exception e) {
+                    return false;
+                }
+            }).collect(Collectors.toList());
         }
         return points;
     }
     
-    public static boolean isWin(Board board){
+    public static boolean isWin(Board board) {
         return false;
     }
     
-    public Board move(Point src, Point dst){
-        //validate first
+    public void move(Point src, Point dst) throws Exception {
+        if (!isMoveAllowed(src, dst)) {
+            throw new Exception("Move not allowed");
+        }
+        _move(src, dst);
+    }
+    
+    public void _move(Point src, Point dst) {
         this.matrix[dst.y][dst.x] = this.matrix[dst.y][dst.x];
         this.matrix[src.y][src.x] = null;
-        return this;
+    }
+    
+    public boolean isMoveAllowed(Point src, Point dst) {
+        ArrayList<Point> allowed = getMoves(src);
+        return allowed.contains(dst);
     }
     
     
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,15 @@
 package com.company;
 
-import com.company.pieces.Bishop;
-import com.company.pieces.Pawn;
-import com.company.pieces.Piece;
-
-import java.awt.*;
+import java.awt.Point;
 import java.util.ArrayList;
 
 public class Main {
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws Exception {
         
         Board board = new Board();
-        ArrayList<Point> points= board.getMoves(new Point(1, 1));
+        ArrayList<Point> points = board.getMoves(new Point(1, 1));
         System.out.println(points);
+        board.move(new Point(1, 1), new Point(1, 2));
     }
 }
